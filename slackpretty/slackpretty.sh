c6145284279ce7993d66db4df3ec15677c5cab07
Dan Benton <dan@dogsbody.com>
2019-08-05 14:43:01 +0100

    Support non 24hr clocks

--- slackpretty/slackpretty.sh
+++ slackpretty/slackpretty.sh
@@ -8,8 +8,8 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ "$1" == "highlighted" ]; then
-    xclip -o -selection primary | perl -0 -lpe 's/[^[:ascii:]]/ /g' | perl -0 -lpe 's|(?:\n\n)?(.*) (\d?\d:\d\d)\n|$2 $1: |gm' | perl -pe 's/^[[:space:]]*$//g' | xclip -i -selection primary
+    xclip -o -selection primary | perl -0 -lpe 's/[^[:ascii:]]/ /g' | perl -0 -lpe 's|(?:\n\n)?(.*) (\d?\d:\d\d(?: [AP]M)?)\n|$2 $1: |gm' | perl -pe 's/^[[:space:]]*$//g' | xclip -i -selection primary
 else
-    xclip -o -selection clipboard | grep -v '^$' | perl -0 -lpe 's|(.*) (\d?\d:\d\d)\n|$2 $1: |gm' | xclip -i -selection clipboard
+    xclip -o -selection clipboard | grep -v '^$' | perl -0 -lpe 's|(.*) (\d?\d:\d\d(?: [AP]M)?)\n|$2 $1: |gm' | xclip -i -selection clipboard
 fi
 
