3c2c5b5f9f25ebcb435f8af68c3c3f424267f253
Rob Hooper <rob@dogsbody.com>
2022-08-12 16:02:59 +0100

    Simple regex upgrade

--- slackpretty/slackpretty.sh
+++ slackpretty/slackpretty.sh
@@ -7,9 +7,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ "$1" == "highlighted" ]; then
-    xclip -o -selection primary | perl -0 -lpe 's/[^[:ascii:]]/ /g' | perl -0 -lpe 's|(?:\n\n)?(.*) (\d?\d:\d\d(?: [AP]M)?)\n|[$2] $1: |gm' | perl -pe 's/^[[:space:]]*$//g' | xclip -i -selection primary
-else
-    xclip -o -selection clipboard | grep -v '^$' | perl -0 -lpe 's|(.*) (\d?\d:\d\d(?: [AP]M)?)\n|[$2] $1: |gm' | xclip -i -selection clipboard
-fi
 
+xclip -o -selection clipboard | perl -0 -lpe '''
+	s/^([0-9:]{5})\n/\n[$1] /gm; # Timestamp matching
+        s/(  (?:[0-9:]{5}|[Tt]oday at [0-9:]{5}|[0-9]+ (?:minutes?|hours?|days?|months?) ago)\n)?(?:\n)?(@[A-Za-z0-9 ]+)\n/$1$2/gm; # At-d users, careful if the first word is an at-d user
+	s/(?:^\n\n|^\n|)(?:[A-Z]+\n|)([A-Za-z0-9 ]+)\n(?:APP|WORKFLOW|:[a-zA-Z0-9]+:|)  ?((?:Today at |Yesterday at |[0-9]+ [A-Za-z]{3} at |)[0-9:]{5}|(?:< |)[0-9]+ (?:minutes?|hours?|days?|months?) ago)\n/[$2] $1: /gm; # Timestamps and names
+	s/(?:^\n\n|^\n|)([0-9]+ repl(?:y|ies))\n(?:Last reply |)([Tt]oday at [0-9:]{5}|[0-9]+ (?:minutes?|hours?|days?|months?) ago)(?:View thread|)(?:\n\n\n\n\n$|$)/($2, $1)/gm; # Thread toggle
+	s/(?:^\n\n|^\n)([0-9]+ repl(?:y|ies))\n/($1)\n/gm; # In thread replie counter
+	s/:\n([0-9]+)\n/: ($1)\n/gm; # Forgot what this is for
+	s/^\n\n\n+//gm; # Random Newlines
+	''' | xclip -i -selection clipboard
