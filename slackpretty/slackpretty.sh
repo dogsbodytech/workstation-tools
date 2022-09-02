78cba259cf56778736e260cf9d213da270890952
Rob Hooper <rob@dogsbody.com>
2022-09-02 09:37:37 +0100

    Extending regex captures

--- slackpretty/slackpretty.sh
+++ slackpretty/slackpretty.sh
@@ -7,8 +7,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-
 xclip -o -selection clipboard | perl -0 -lpe '''
+	s/^(replied to a thread:)\n(.*)\n/($1 $2)\n/gm; # Replied to thread in channel
 	s/^([0-9:]{5})\n/\n[$1] /gm; # Timestamp matching
         s/(  (?:[0-9:]{5}|[Tt]oday at [0-9:]{5}|[0-9]+ (?:minutes?|hours?|days?|months?) ago)\n)?(?:\n)?(@[A-Za-z0-9 ]+)\n/$1$2/gm; # At-d users, careful if the first word is an at-d user
 	s/(?:^\n\n|^\n|)(?:[A-Z]+\n|)([A-Za-z0-9 ]+)\n(?:APP|WORKFLOW|:[a-zA-Z0-9]+:|)  ?((?:Today at |Yesterday at |[0-9]+ [A-Za-z]{3} at |)[0-9:]{5}|(?:< |)[0-9]+ (?:minutes?|hours?|days?|months?) ago)\n/[$2] $1: /gm; # Timestamps and names
